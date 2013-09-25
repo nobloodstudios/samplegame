0cd8e543ca5ea0ce503143dcee5235ce0ddd454d
Iulia Mogos <iulia.mogos@king.com>
2013-09-25 18:30:21 +0300

    filter

---
+++ change-commiter.sh
@@ -0,0 +1,1 @@
+git filter-branch -f --env-filter "GIT_AUTHOR_NAME='nobloodstudios'; GIT_AUTHOR_EMAIL='nobloodstudios@yahoo.com'; GIT_COMMITTER_NAME='nobloodstudios'; GIT_COMMITTER_EMAIL='nobloodstudios@yahoo.com';" HEAD
